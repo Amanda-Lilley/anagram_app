a77f808d7f4b71015c9eb08bac393142ba6c1d3d
Amanda Lilley <yoknitting24@gmail.com>
2016-01-31 15:32:59 -0500

    fixed index controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -13,6 +13,7 @@ end
 get '/anagrams/:word' do
   @word = params[:word]
   @length = @word.length
+  @perms = (1..@word.length).inject(:*)
   @anagram_results = Word.anagram_generator(@word)
   erb :show
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -3,17 +3,11 @@ class Word < ActiveRecord::Base
 
 
   def self.anagram_generator(word)
-
     @length = word.length
-    #puts "Your word has #{@length} characters."
-
     @perms = (1..@length).inject(:*)
-    #puts "There are #{perms} permutations for your chosen word"
-
     @anagram_results = word.chars.to_a.permutation.map(&:join)
   #  @anagram_results.each { |x| puts x }
      return @anagram_results
-
   end
 
 end
