e3eca35799cd8b7185507f7706c5d082c590024b
Amanda Lilley <yoknitting24@gmail.com>
2016-02-10 08:33:54 -0500

    updated database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118154737) do
+ActiveRecord::Schema.define(version: 20160206002832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "words", force: :cascade do |t|
-    t.string   "text"
+    t.string "text"
   end
 
 end
